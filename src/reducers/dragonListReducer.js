0d7a820e6b504c8c13b48f4aa5d1161df4853c18
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-17 18:25:54 -0800

    lecture work-in-progress

--- src/reducers/dragonListReducer.js
+++ src/reducers/dragonListReducer.js
@@ -2,12 +2,14 @@ const initialState = {
   members: [
     { name: 'Jojo Zhang', dragonStatus: true },
     { name: 'Brandon Harris', dragonStatus: false }
+    { name: 'Josh J', dragonStatus: true }
   ]
 };
 
 export const dragonListReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_MEMBER":
+      const newMember = {name: action.payload, dragonStatus: false}
       return {
         ...state, 
         members: [...state.members, action.payload]
