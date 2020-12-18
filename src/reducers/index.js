1e35251c62d071da437dbb75f2ce857f3630fee5
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-17 18:31:34 -0800

    lecture work-in-progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
 import {combineReducers} from "react-redux";
+import {titleReducer} from "./titleReducer";
+import {dragonListReducer} from "./dragonListReducer";
 
-import {titleReducer} from "./titleReducer"
+export const rootReducer = combineReducers({
+    dragonList: dragonListReducer,
+    title: titleReducer
+})
