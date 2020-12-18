0f0a84cbca0d91ce144520666c020194e659e445
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-17 18:01:22 -0800

    lecture work-in-progress

--- src/reducers/titleReducer.js
+++ src/reducers/titleReducer.js
@@ -6,12 +6,14 @@ export const initialState = {
   export const titleReducer = (state = initialState, action) => {
     switch (action.type) {
       case "UPDATE_TITLE":
+        console.log("update title in the reducer");
         return {
           ...state,
           title: action.payload,
           editing: false
         };
       case "TOGGLE_EDITING":
+        console.log("toggle editing in the reducer");
         return {
           ...state,
           editing: !state.editing
