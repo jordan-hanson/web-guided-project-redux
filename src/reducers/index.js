8ab786e09e23c24d4fbeeeb19bd105df4b6568a2
Josh Jacobson <joshjacobson14@gmail.com>
2020-12-17 18:34:02 -0800

    lecture work-in-progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import {combineReducers} from "react-redux";
+import {combineReducers} from "redux";
 import {titleReducer} from "./titleReducer";
 import {dragonListReducer} from "./dragonListReducer";
 
